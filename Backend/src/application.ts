64ab2cf742769b9d20bd68096901f9ea83263faf
Ezequiel Reale <ezequiel.reale192436@alumnos.info.unlp.edu.ar>
2026-06-11 15:52:14 -0300

    fix: actualizacion de dependencias y configuracion

--- Backend/src/application.ts
+++ Backend/src/application.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { BootMixin } from '@loopback/boot';
 import { ApplicationConfig, createBindingFromClass } from '@loopback/core';
 import {
